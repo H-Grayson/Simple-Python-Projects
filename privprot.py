6d602f3cbef8a2d0b274682b2fbcd38c6d29cbe1
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-05-30 12:50:21 -0600

    Update privprot.py

--- privprot.py
+++ privprot.py
@@ -1,31 +1,57 @@
-class Revenue:
+import os
+import time
+import shutil
+import tkinter
+from tkinter import *
+from tkinter import ttk, filedialog
+from tkinter.filedialog import askdirectory
+import pathlib
+from pathlib import Path
 
-    def init(self, x, y):
-        #protected property
-        self._twitchRev = 0
-        #private property
-        self.__youtubeRev = 0
+
+root = Tk()
+root.title("Select folder to be cleared after 24 hours of inactivity")
+root.geometry("350x350")
+
+
+def sbutton():
+    global folderChecked
+    folderChecked = filedialog.askdirectory(parent=root, initialdir="/", title="Select Dir")
+    
+    print(folderChecked)
         
-    #function that take x and assigns it to "__youtubeRev"
-    def stream1(self, x):
-        self.__youtubeRev1 = x
+    
+    #button targeting where files last modified >= 24 hours ago will go
+def fbutton():
+    global folderTarget
+    folderTarget = filedialog.askdirectory(parent=root, title='Choose a file')
+    print("Sending to " + folderTarget)
 
-    #method to get the value of the "__youtubeRev" private property
-    def getYoutubeRev(self):
-        return self.__youtubeRev
+def sortbutton():
+    # if folder ends with .txt and getmtime >= 24 hours move files from folderChecked to folderTarget
+    for files in folderChecked:
+        if folderChecked.endswith(".txt") and os.path.getmtime(filename) >= (time.time() - 60*60*24):
+            shutil.move(os.path.join(folderChecked, filename), folderTarget)
+        
+    else: print("No files needing moved")
 
-    #function that takes y and assigns it to "_twitchRev"
-    def stream2(self, y):
-        self._twitchRev = y
+def testButton():
+    print(folderChecked)
 
-#instantiates a "Revenue" object name "streaming"
-streaming = Revenue()
-#assigns a value of 20000 to "__youtubeRev"
-streaming.stream1(20000)
-#assigns a value of 30000 to "__twitchRev"
-streaming.stream2(30000)
+def testButton1():
+    print(folderTarget)
+ 
+label = Label(root, text="Choose folder to be checked daily.")
+label.pack(pady=10)
 
-# add youtuberev and twitch rev properties
-totalRev = streaming.getYoutubeRev() + streaming._twitchRev
+# button used to choose folder to scan for .txt files
+browseB = ttk.Button(root, text="From", command=sbutton).pack(pady=20)
 
-print(totalRev)
+# button used to select where the .txt files will go
+selectB = ttk.Button(root, text="To", command=fbutton).pack(pady=30)
+
+# button used to perform sort action
+sortB = ttk.Button(root, text="Sort", command = sortbutton).pack(pady=30)
+
+testb1 = ttk.Button(text="sending", command=testButton).pack()
+testb2 = ttk.Button(text="recieving", command=testButton1).pack()
