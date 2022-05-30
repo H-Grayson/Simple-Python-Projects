04fb8a72889b5bde340b14938cac5e4bd00c2e53
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-05-30 14:29:07 -0600

    Update privprot.py

--- privprot.py
+++ privprot.py
@@ -5,8 +5,7 @@ import tkinter
 from tkinter import *
 from tkinter import ttk, filedialog
 from tkinter.filedialog import askdirectory
-import pathlib
-from pathlib import Path
+
 
 
 root = Tk()
@@ -14,43 +13,44 @@ root.title("Select folder to be cleared after 24 hours of inactivity")
 root.geometry("350x350")
 
 
+    #button targeting where files will be selected from
 def sbutton():
-    global folderChecked
-    folderChecked = filedialog.askdirectory(parent=root, initialdir="/", title="Select Dir")
-    print(folderChecked)
+    folderChecked = filedialog.askdirectory()
+    sourceDirectory.insert(0,folderChecked)
         
     
     #button targeting where files last modified >= 24 hours ago will go
 def fbutton():
-    global folderTarget
-    folderTarget = filedialog.askdirectory(parent=root, title='Choose a file')
-    print("Sending to " + folderTarget)
+    folderTarget = filedialog.askdirectory()
+    destinationDirectory.insert(0,folderTarget)
 
 def sortbutton():
-    # if folder ends with .txt and getmtime >= 24 hours move files from folderChecked to folderTarget
-    for file in folderChecked:
-        if folderChecked.endswith(".txt") and os.path.getmtime(filename) >= (time.time() - 60*60*24):
-            shutil.move(os.path.join(folderChecked, filename), folderTarget)
-        
-    else: print("No files needing moved")
+    source = sourceDirectory.get()
+    destination = destinationDirectory.get()
+    source_files = os.listdir(source)
+    # if files in folder are getmtime >= 24 hours move files from folderChecked to folderTarget
+    for file in source_files:
+        filepath = os.path.join(source, file)
+        if os.path.getmtime(filepath) >= (time.time() - 60*60*24):
+            shutil.move(source + '/' + file, destination)
 
-def testButton():
-    print(folderChecked)
-
-def testButton1():
-    print(folderTarget)
- 
 label = Label(root, text="Choose folder to be checked daily.")
-label.pack(pady=10)
+label.grid(row=0, column=0, padx=(20,10), pady=(30,0))
 
 # button used to choose folder to scan for .txt files
-browseB = ttk.Button(root, text="From", command=sbutton).pack(pady=20)
+browseB = ttk.Button(root, text="From", command=sbutton)
+browseB.grid(row=1, column=0, padx=(20,10), pady=(30,0))
+sourceDirectory = Entry(root, width=100)
+sourceDirectory.grid(row=2, column=0, padx=(20,10), pady=(30,0))
 
 # button used to select where the .txt files will go
-selectB = ttk.Button(root, text="To", command=fbutton).pack(pady=30)
-
+selectB = ttk.Button(root, text="To", command=fbutton)
+selectB.grid(row=3, column=0, padx=(20,10), pady=(30,0))
+destinationDirectory = Entry(root, width=100)
+destinationDirectory.grid(row=4, column=0, padx=(20,10), pady=(30,0))
+                          
 # button used to perform sort action
-sortB = ttk.Button(root, text="Sort", command = sortbutton).pack(pady=30)
+sortB = ttk.Button(root, text="Sort", command=sortbutton)
+sortB.grid(row=5, column=0, padx=(20,10), pady=(30,0))
 
-testb1 = ttk.Button(text="sending", command=testButton).pack()
-testb2 = ttk.Button(text="recieving", command=testButton1).pack()
+root.mainloop()
