dfd43b011d910519316e23cf192489d594b06d6a
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-05-31 14:08:41 -0600

    Update privprot.py

--- privprot.py
+++ privprot.py
@@ -2,14 +2,16 @@ class Revenue:
 
     def init(self, x, y):
         self._twitchRev = 0
+        
+        self._youtubeRev = 0
 
-    def stream1(self):
+    def stream1(self, x):
         self.__youtubeRev = x
 
     def getYoutubeRev(self):
         return self.__youtubeRev
     
-    def stream2(self):
+    def stream2(self, y):
         self._twitchRev = y
 
 streaming = Revenue()
